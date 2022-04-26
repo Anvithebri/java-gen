344845043de90ec0c75bca347ca2ee043765900c
anvithebri <anvithebri@gmail.com>
2022-04-26 16:12:31 +0530

    inheritance

---
+++ src/Inheritance.java
@@ -0,0 +1,17 @@
+class c{
+    int a;
+    void show(){
+        System.out.println("from show function");
+    }
+}
+public class Inheritance extends c {
+    public void display(){
+        System.out.println("from mdisplay");
+    }
+    public static void main(String[] args){
+        System.out.println(args[0]+" "+args[1]);
+       c obj= new c();
+       obj.show
+    }
+
+}
